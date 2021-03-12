e8777713c101c7e5a6951557464b14ed78f23437
danielraduna <62072227+danielraduna@users.noreply.github.com>
2021-03-13 00:16:43 +0200

    Update otp.asm

--- otp.asm
+++ otp.asm
@@ -5,7 +5,7 @@ section .text
     extern printf
 
 otp:
-    ;; DO NOT MODIFY
+   
     push    ebp
     mov     ebp, esp
     pusha
@@ -14,17 +14,15 @@ otp:
     mov     esi, [ebp + 12]     ; plaintext
     mov     edi, [ebp + 16]     ; key
     mov     ecx, [ebp + 20]     ; length
-    ;; DO NOT MODIFY
-
-    ;; TODO: Implement the One Time Pad cipher
+    
 while:
 	mov al, [esi + ecx  -1]  ;;pun in al litera din plaintext
 	mov ah, [edi + ecx - 1]  ;;pun in ah litera din key
 	xor al, ah				 ;;fac xor intre cele doua litere
 	mov [edx + ecx - 1], al	 ;;pun in ciphertext rezultatul operatiei xor
 	loop while				
-    ;; DO NOT MODIFY
+   
     popa
     leave
     ret
-    ;; DO NOT MODIFY
+    
